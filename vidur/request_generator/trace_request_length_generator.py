dca46004ba6581e1b25ab3f3c5d4b6e356026f7e
JonnyKong <jonnykong1996@gmail.com>
2025-03-04 03:39:25 +0000

    Make request length generator wrap around so it doesn't exhaust

--- vidur/request_generator/trace_request_length_generator.py
+++ vidur/request_generator/trace_request_length_generator.py
@@ -89,7 +89,7 @@ class TraceRequestLengthGenerator(BaseRequestLengthGenerator):
 
     def get_next_num_tokens(self) -> Tuple[float, float]:
         if self.next_request_idx >= len(self.trace_df):
-            return None, None
+            self.next_request_idx = 0
 
         row = self.trace_df.iloc[self.next_request_idx]
         self.next_request_idx += 1
